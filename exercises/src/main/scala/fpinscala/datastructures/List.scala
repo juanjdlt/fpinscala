37349d8d360d408f1a5f5780885c614938944c1d
Juan Jose de la Torre <juanjdlt@gmail.com>
2022-11-15 20:28:22 -0500

    DataStructures exercies until 3.8

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -1,68 +1,137 @@
 package fpinscala.datastructures
 
 sealed trait List[+A] // `List` data type, parameterized on a type, `A`
-case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
+case object Nil
+    extends List[
+      Nothing
+    ] // A `List` data constructor representing the empty list
 /* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
 which may be `Nil` or another `Cons`.
  */
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List { // `List` companion object. Contains functions for creating and working with lists.
-  def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
-    case Nil => 0 // The sum of the empty list is 0.
-    case Cons(x,xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
-  }
+  def sum(ints: List[Int]): Int =
+    ints match { // A function that uses pattern matching to add up a list of integers
+      case Nil => 0 // The sum of the empty list is 0.
+      case Cons(x, xs) =>
+        x + sum(
+          xs
+        ) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
+    }
 
   def product(ds: List[Double]): Double = ds match {
-    case Nil => 1.0
+    case Nil          => 1.0
     case Cons(0.0, _) => 0.0
-    case Cons(x,xs) => x * product(xs)
+    case Cons(x, xs)  => x * product(xs)
   }
 
   def apply[A](as: A*): List[A] = // Variadic function syntax
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
-  val x = List(1,2,3,4,5) match {
-    case Cons(x, Cons(2, Cons(4, _))) => x
-    case Nil => 42
+  val x = List(1, 2, 3, 4, 5) match {
+    case Cons(x, Cons(2, Cons(4, _)))          => x
+    case Nil                                   => 42
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-    case Cons(h, t) => h + sum(t)
-    case _ => 101
+    case Cons(h, t)                            => h + sum(t)
+    case _                                     => 101
   }
 
   def append[A](a1: List[A], a2: List[A]): List[A] =
     a1 match {
-      case Nil => a2
-      case Cons(h,t) => Cons(h, append(t, a2))
+      case Nil        => a2
+      case Cons(h, t) => Cons(h, append(t, a2))
     }
 
-  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
+  def foldRight[A, B](
+      as: List[A],
+      z: B
+  )(f: (A, B) => B): B = // Utility functions
     as match {
-      case Nil => z
+      case Nil         => z
       case Cons(x, xs) => f(x, foldRight(xs, z)(f))
     }
 
+  // foldRight(List(1,2,3), Nil[List[Int]])((x, y) => Cons(x, y))
+
+  // Cons(1, Cons(2, Cons(3, Nil)))
+
   def sum2(ns: List[Int]) =
-    foldRight(ns, 0)((x,y) => x + y)
+    foldRight(ns, 0)((x, y) => x + y)
 
   def product2(ns: List[Double]) =
-    foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
+    foldRight(ns, 1.0)(
+      _ * _
+    ) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil         => Nil
+    case Cons(_, xs) => xs
+  }
 
-  def tail[A](l: List[A]): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil         => Nil
+    case Cons(_, xs) => Cons(h, xs)
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] = {
+    if (n > 0) {
+      l match {
+        case Nil => Nil
+        case Cons(_, xs) =>
+          drop(xs, n - 1)
+      }
+    } else {
+      l
+    }
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  // Currying this function so we can help the compiler to infer the anon funtion type when using it
+  def dropWhile[A](l: List[A])(f: A => Boolean): List[A] = {
+    l match {
+      case Cons(h, t) if f(h) => dropWhile(t)(f)
+      case _                  => l
+    }
+  }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  // Note that we're copying the entire list up until the last element. Besides being inefficient, the natural recursive solution will use a stack frame for each element of the list, which can lead to stack overflows for large lists (can you see why?). With lists, it's common to use a temporary, mutable buffer internal to the function (with lazy lists or streams, which we discuss in chapter 5, we don't normally do this). So long as the buffer is allocated internal to the function, the mutation is not observable and RT is preserved.
+  def init[A](l: List[A]): List[A] = {
+    l match {
+      case Nil          => sys.error("init of empty list")
+      case Cons(_, Nil) => Nil
+      case Cons(x, xs)  => Cons(x, init(xs))
+    }
+  }
 
-  def init[A](l: List[A]): List[A] = ???
+  def length[A](l: List[A]): Int =
+    foldRight(l, 0)((_, y) => y + 1)
 
-  def length[A](l: List[A]): Int = ???
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = ???
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  def map[A, B](l: List[A])(f: A => B): List[B] = ???
+}
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = ???
+object TestListFunctions {
+
+  import List._
+  def main(args: Array[String]): Unit = {
+    val emptyList = Nil
+    val l = List(1, 2, 3, 4, 5, 0, 6, 7, 8, 9)
+
+    println("ex1: " + List.x)
+    println("ex2: " + List.tail(emptyList))
+    println("setHead Test :: " + List.setHead(l, 0))
+    println("drop() Test :: " + List.drop(l, 8))
+    println("dropWhile() Test :: " + List.dropWhile(l)(x => x >= 1))
+    println("init() test :: " + List.init(List(1, 2, 3, 4, 5)))
+    println(
+      "FoldRight 3.8() test :: " + List.foldRight(
+        List(1, 2, 3),
+        Nil: List[Int]
+      )(Cons(_, _))
+    )
+    println("Lengt() test :: " + List.length(List(1, 1, 1, 1, 1)))
+
+  }
 }
